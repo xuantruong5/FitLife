54d990e82cc4b455a4213606b2de424d8384bb95
xuantruongne <truongdeptrai081205@gmail.com>
2026-06-21 21:21:15 +0700

    cautruc

--- App.tsx
+++ App.tsx
@@ -1,45 +1,116 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer, createNavigationContainerRef } from "@react-navigation/native";
+import { Alert, Animated } from "react-native";
+import React, { useEffect, useState } from "react";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import HomePage from "./src/pages/HomePage";
+import { notificationEmitter } from "./src/general/notification";
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
 
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+const navigationRef = createNavigationContainerRef();
+
+
+function AnimatIcon({ name_icon, focused, color, size }: { name_icon: string; focused: boolean; color: string; size: number }) {
+  const scaleIcon = new Animated.Value(focused ? 1.2 : 1);
+  React.useEffect(() => {
+    Animated.spring(scaleIcon, {
+      toValue: focused ? 1.2 : 1,
+      useNativeDriver: true,
+    }).start();
+  }, [focused]);
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <Animated.View style={{ transform: [{ scale: scaleIcon }] }}>
+      <Ionicons name={name_icon} size={size} color={color} />
+    </Animated.View>
   );
 }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
+function MainTabs() {
   return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ focused, color, size }) => {
+          let name_icon = "";
+          if (route.name === "Home") name_icon = focused ? "home" : "home-outline";
+          else if (route.name === "Wishlist") name_icon = focused ? "heart-sharp" : "heart-outline";
+          else if (route.name === "ScanQR") name_icon = focused ? "scan-circle" : "scan";
+          else if (route.name === "ChatBot") name_icon = focused ? "chatbubbles" : "chatbubbles-outline";
+          else if (route.name === "Profile") name_icon = focused ? "person-circle-sharp" : "people-outline";
+          return (
+            <AnimatIcon
+              name_icon={name_icon}
+              focused={focused}
+              color={color}
+              size={size}
+            />
+          );
+        },
+        tabBarActiveTintColor: "black",
+        tabBarInactiveTintColor: "gray",
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: "bold"
+        }
+      })}
+    >
+      <Tab.Screen name="Home" component={HomePage} />
+      {/* <Tab.Screen name="Wishlist" component={Wishlist} />
+            <Tab.Screen name="ScanQR" component={ScanQR} />
+            <Tab.Screen name="ChatBot" component={ChatBot} />
+            <Tab.Screen name="Profile" component={Profile} /> */}
+
+    </Tab.Navigator>
+
   );
 }
+const App = () => {
+      const [pendingRoute, setPendingRoute] = useState<string | null>(null);
+      useEffect(() => {
+        const sub = (payload: any) => {
+          const { type, message } = payload;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+          // Tạm thời chỉ hiện thông báo
+          Alert.alert("Thông báo", message);
+        };
+
+        notificationEmitter.on("thong_bao", sub);
+
+        return () => {
+          notificationEmitter.off("thong_bao", sub);
+        };
+      }, []);
+      return (
+        <NavigationContainer
+              ref={navigationRef}
+              onReady={()=>{
+                if(pendingRoute){
+                  navigationRef.navigate(pendingRoute as never);
+                  setPendingRoute(null);
+                }
+              }}
+          >
+            <Stack.Navigator
+                  initialRouteName="MainTabs"
+                  screenOptions={{headerShown:false}}
+                  >
+                    <Stack.Screen name="MainTabs" component={MainTabs} />
+                    
+
+
+
+            </Stack.Navigator>
+
+        </NavigationContainer>
+      )
+}
+
+
+
+
 
 export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native"
+
+const HomePage = () => {
+    return (
+        <View>
+            <Text>tra my </Text>
+        </View>
+    )
+}
+export default HomePage
