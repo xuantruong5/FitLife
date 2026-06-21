7b21e196e9a92b46002ac12c19041abfd3ccad0a
xuantruongne <truongdeptrai081205@gmail.com>
2026-06-21 21:25:08 +0700

    day cau truc

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,11 @@ import { Text, View } from "react-native"
 
 const HomePage = () => {
     return (
-        <View>
+        <View style={{
+            flex:1,
+            justifyContent:"center",
+            alignItems:"center"
+        }}>
             <Text>tra my </Text>
         </View>
     )
