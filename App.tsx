0655dbe7f724934cdf08dd2a209416854a4665e4
xuantruongne <truongdeptrai081205@gmail.com>
2026-07-15 20:05:39 +0700

    lam giao dien tao lich day cho member

--- App.tsx
+++ App.tsx
@@ -45,7 +45,6 @@ import MemberChangeSchedule from "./src/pages/Member/MemberChangeSchedule";
 import TrainerEditProfile from "./src/pages/TrainerEditProfile";
 import TrainerChangePassword from "./src/pages/TrainerChangePassword";
 import TrainerTeachingHistory from "./src/pages/TrainerTeachingHistory";
-import StoreScheduleMember from "./src/pages/StoreScheduleMember";
 
 
 
@@ -99,7 +98,7 @@ function MainTabs() {
       })}
     >
       <Tab.Screen name="Home" component={HomePage} />
-      <Tab.Screen name="tao lich" component={StoreScheduleMember} />
+      {/* <Tab.Screen name="login" component={Login} /> */}
       {/* <Tab.Screen name="ScanQR" component={ScanQR} />
       <Tab.Screen name="ChatBot" component={ChatBot} /> */}
       <Tab.Screen name="Profile" component={Profile} />
